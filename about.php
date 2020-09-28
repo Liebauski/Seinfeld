e4548f21412fb0c7b2b967b68b018881847384db
Jacob Liebau <jacobliebau@gmail.com>
2020-09-27 20:38:53 -0400

    Added login/logout features for Lab 4

---
+++ about.php
@@ -0,0 +1,57 @@
+<?php 
+require "includes/header.php"
+?>
+<main>
+<link rel="stylesheet" href="css/about.css">
+    <div class="about">
+        <h1>About Us</h1>
+        <p>Seinfeld of the 90s</p>
+        <p>Here's the team that brought you the hit sitcom SEINFELD!</p>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/jerry.jpg">
+                <div class="container">
+                    <h2>The Funny Guy</h2>
+                    <p>Jerry Seinfeld</p>
+                    <p>Email: jerry@aol.com</p>
+                    <button>Click Jerry!</button>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/elaine.jpg">
+                <div class="container">
+                    <h2>The Writer</h2>
+                    <p>Elaine Benes</p>
+                    <p>Email: jaypeterman@aol.com</p>
+                    <button>Click Elaine!</button>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/george.jpg">
+                <div class="container">
+                    <h2>The Architect</h2>
+                    <p>George Costanza</p>
+                    <p>Email: festivus@aol.com</p>
+                    <button>Don't Click George</button>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/kramer.jpg">
+                <div class="container">
+                    <h2>The "Entrepreneur"</h2>
+                    <p>Cosmo Kramer</p>
+                    <p>Email: crazycosmo@aol.com</p>
+                    <button>Click Kramer!</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
